245145110472345c7be76aa1b9c5eefc163e701b
Vladimir Rancic <vladimir.rancic@np011.se>
2023-05-20 22:03:25 +0200

    remove old and add new Next.js application from scratch

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: [
-    "@fullcalendar/common",
-    "@babel/preset-react",
-    "@fullcalendar/common",
-    "@fullcalendar/daygrid",
-    "@fullcalendar/interaction",
-    "@fullcalendar/react",
-    "@fullcalendar/timegrid",
-    "react-github-btn",
-    "dropzone",
-  ],
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboards/analytics",
-        permanent: true,
-      },
-    ];
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
